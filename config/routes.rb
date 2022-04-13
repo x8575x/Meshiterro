8dc8a8b845fb93ff866dc0bd043e3d00caafbc50
EC2 Default User <ec2-user@ip-172-31-22-189.ec2.internal>
2022-04-13 02:30:37 +0000

    [Finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
 
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+
     resources :post_comments, only: [:create, :destroy]
   end
-  
-  
+
+
   resources :users, only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
